f9cd23e360777c1b3e536d43c7dc920be20247f6
LinDevHard <lindevhard@gmail.com>
2019-06-30 02:17:01 +0600

    ViewModel Factory created

---
+++ app/src/main/java/com/hackware/mormont/raspbfinder/ViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.hackware.mormont.raspbfinder
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.hackware.mormont.raspbfinder.net.NetManager
+
+
+class ViewModelFactory(
+    private val netManager: NetManager
+) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>) =
+        with(modelClass) {
+            when {
+                isAssignableFrom(MainViewModel::class.java) ->
+                    MainViewModel(netManager)
+                else ->
+                    throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+            }
+        } as T
+
+}
